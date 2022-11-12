f77e0ae5ccb74b043e2630670b52e1793aa3a904
Abdulkadir Fındık <65927031+TmihiW@users.noreply.github.com>
2022-11-12 21:33:42 +0300

    Add files via upload

---
+++ 01_output.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Output Page</title>
+  <a href="00_main.php">|Main Page|</a><hr></hr>
+  <a href="01_output.php">|Output Page|</a>
+  <a href="02_variables.php">|Variables Page|</a>
+  <a href="03_arrays.php">|Arrays Page|</a>
+  <a href="04_conditionals.php">|Conditionals Page|</a>
+  <a href="05_loops.php">|Loops Page|</a>
+  <a href="06_functions.php">|Functions Page|</a>
+  <a href="07_array_functions.php">|Array Functions Page|</a>
+  <a href="08_string_functions.php">|String Functions Page|</a>
+  <hr></hr>
+</head>
+<body>
+
+  <br></br>
+  <h1>Merhaba <?php echo 'Hello'; ?></h1>
+  <?php if (false):?> <h1>Hello1 <?php echo 'Merhaba'; ?></h1>
+	<?php elseif(true):?><h3>Hello2 <?php echo 'Merhaba'; ?></h3>
+	<?php endif;?>
+  
+  <h2>Hello <?= 'Merhaba' ?></h2>
+
+  <?php for ($i = 0; $i < 5; ++$i): ?>
+     <h3> hi</h3>
+	<?php endfor; ?>
+	
+	<br></br>
+<?php
+
+	echo " Hello World!   ";
+?>
+<br></br>
+<?php
+
+	echo 123, ' ABC' , ' a' ; 
+?>
+<br></br>
+
+<?php
+
+
+	//never gona use probably
+	print ' Merhaba';
+	print_r(' Hello ');
+?>
+<br></br>
+<?php
+	//print_r ile diziler yazdırılabilir
+	echo'<hr>print_r([1,2,3]);<br>'.'output:<br>';
+	print_r([1,2,3]);
+?>
+<br></br>
+<?php
+    //değişken tipi ve uzunluğunuda döndürür
+	echo'<hr>var_dump(\'Merhaba\');<br>'.'output:<br>';
+	var_dump('Merhaba');
+?>
+<br></br>
+<?php
+echo'<hr>var_dump(true);<br>'.'output:<br>';
+	var_dump(true);
+?>
+<br></br>
+<?php
+echo'<hr>var_export(\'Hello\');<br>'.'output:<br>';
+	var_export('Hello');
+
+?>
+<hr></hr>
+</body>
+
+</html>
+
+
+
+	
