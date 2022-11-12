adf970f96ee17b544b48343053bf0f6b5d161bae
Abdulkadir Fındık <65927031+TmihiW@users.noreply.github.com>
2022-11-12 21:32:42 +0300

    Add files via upload

---
+++ 00_main.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>My PHP Website</title>
+  <a href="00_main.php">|Main Page|</a><hr></hr>
+  <a href="01_output.php">|Output Page|</a>
+  <a href="02_variables.php">|Variables Page|</a>
+  <a href="03_arrays.php">|Arrays Page|</a>
+  <a href="04_conditionals.php">|Conditionals Page|</a>
+  <a href="05_loops.php">|Loops Page|</a>
+  <a href="06_functions.php">|Functions Page|</a>
+  <a href="07_array_functions.php">|Array Functions Page|</a>
+  <a href="08_string_functions.php">|String Functions Page|</a>
+  <hr></hr>
+</head>
+<body>
+  
+<br></br>
+<?php
+echo 'Welcome to PHP Crash';
+
+?>
+
+<br></br>
+<?php
+echo'output:<br>';
+?>
+<hr></hr>
+<br></br>
+
+</body>
+</html>
